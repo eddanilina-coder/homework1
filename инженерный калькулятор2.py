d8fd63bb9490043fc5e9464595fb6d8b22b5c9eb
eddanilina-coder <e.d.danilina@gmail.com>
2025-10-09 22:23:17 +0300

    task1

---
+++ инженерный калькулятор2.py
@@ -0,0 +1,47 @@
+import math
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            return "Ошибка: Деление на ноль"
+        return a / b
+
+    def power(self, a, b):
+        return a ** b
+
+    def sqrt(self, a):
+        if a < 0:
+            return "Ошибка: Корень из отрицательного числа"
+        return math.sqrt(a)
+
+    def sin(self, angle_degrees):
+        return math.sin(math.radians(angle_degrees))
+
+    def cos(self, angle_degrees):
+        return math.cos(math.radians(angle_degrees))
+
+    def tan(self, angle_degrees):
+        return math.tan(math.radians(angle_degrees))
+
+    def log(self, a): # Натуральный логарифм
+        if a <= 0:
+            return "Ошибка: Логарифм не положительного числа"
+        return math.log(a)
+
+    def log10(self, a): # Десятичный логарифм
+        if a <= 0:
+            return "Ошибка: Логарифм не положительного числа"
+        return math.log10(a)
+    
